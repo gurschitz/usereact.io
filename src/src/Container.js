c43dc62cb157b52ff17708b8d497bb9ac68dea8e
Gerald Urschitz <gerald.urschitz@gmail.com>
2018-11-15 15:41:26 +0100

    some improvements

---
+++ src/src/Container.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+const Container = styled.div`
+  padding-left: 15px;
+  padding-right: 15px;
+  overflow-x: hidden;
+  display: flex;
+  height: 100%;
+  color: white;
+  padding-top: 30px;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export default Container;
---
+++ src/src/Person/Name.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const Name = styled.h2`
+  font-weight: bold;
+  font-size: 20px;
+  margin-bottom: 5px;
+  color: #a68ed3;
+`;
+
+export default Name;
